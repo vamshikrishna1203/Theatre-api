eb5de1f9a57b261ba0795c04abbf3f79356d2fbd
vamshi <vamshiguptha1203@gmail.com>
2021-03-08 20:19:33 +0530

    feat: Add get_object_by_seat, get_object_by_ticket methods

--- managerapp/mixins.py
+++ managerapp/mixins.py
@@ -1,7 +1,9 @@
-from consumer_app.models import Seat,Consumer
+from consumer_app.models import Seat,Customer
 from django.conf import settings
 from django.http import HttpResponse
 class RetriveMixin(object):
+
+
     def get_data(self,seat):
         data = {
             'name': seat.consumer.u_name,
@@ -9,3 +11,15 @@ class RetriveMixin(object):
             'seat_no': seat.seat_no
         }
         return data
+
+    def get_object_by_seat(self,seat_no):
+        con = Seat.objects.get(seat_no = seat_no)
+        return self.get_data(con)
+
+    def get_object_by_ticket(self,ticket_id):
+        lis = []
+        con = Seat.objects.all()
+        for i in con:
+            if str(i.customer.ticket_id) == ticket_id:
+                lis.append(self.get_data(i))
+        return lis
