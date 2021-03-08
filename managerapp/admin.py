76de00dbe255357820442747e6d56547f156a2d8
vamshi <vamshiguptha1203@gmail.com>
2021-03-08 23:52:19 +0530

    style: add whitespace, blanklines nad remove unexpected whitespaces

--- managerapp/admin.py
+++ managerapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from managerapp.models import Customer, Seat
 
-#Register models here
-admin.site.register(Customer) 
+# Register models here
+admin.site.register(Customer)
 admin.site.register(Seat)
-
