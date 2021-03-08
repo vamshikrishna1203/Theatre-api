1f0c9523d2aeeb90e36ffc3dc0dc83573e04c576
vamshi <vamshiguptha1203@gmail.com>
2021-03-08 21:43:36 +0530

    feat: add get_seat_no method

--- managerapp/mixins.py
+++ managerapp/mixins.py
@@ -43,3 +43,13 @@ class OccupyMixin(object):
         obj = Customer.objects.get(ticket_id = data['ticket_id'])
         if(obj.name == data['name']):
             return obj
+
+    def get_seat_no(self):
+        print('in method')
+        for seat_no in range(1,settings.MAX_CAPACITY+1):
+            print(seat_no)
+
+            if  not Seat.objects.filter(seat_no = seat_no).exists():
+                print(seat_no)
+                return seat_no
+        return settings.MAX_CAPACITY + 1
