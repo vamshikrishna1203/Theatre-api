53cc21a1511114b5b179e032a7ee632fb8f278f4
vamshi <vamshiguptha1203@gmail.com>
2021-03-10 00:01:24 +0530

    style: add comments

--- managerapp/tests.py
+++ managerapp/tests.py
@@ -1,12 +1,12 @@
-from .models import Customer,Seat
+from .models import Customer, Seat
 from django.test import TestCase
-import requests, json
+import requests
+import json
 from managerapp.testfunctions import *
 BASE_URL = 'http://127.0.0.1:8000/'
 
 
-
-class BookSeatTest(TestCase,OccupyTestFuctions):
+class BookSeatTest(TestCase, OccupyTestFuctions):
     """Test class for seat reservaiton"""
 
     def test_create_valid_reservation(self):
@@ -14,7 +14,7 @@ class BookSeatTest(TestCase,OccupyTestFuctions):
         # Provide values of objects in currnet model
         valid_name = 'vamshi'
         valid_ticket = '2e6e79c4-aa80-406a-9517-9b75552eb7ed'
-        response = self.create_valid_resource(valid_name,valid_ticket)
+        response = self.create_valid_resource(valid_name, valid_ticket)
         self.assertEqual(response.status_code, 200)
 
     def test_create_invalid_reservation(self):
@@ -23,31 +23,39 @@ class BookSeatTest(TestCase,OccupyTestFuctions):
         valid_ticket = '2e6e79c4-aa80-406a-9517-9b75552eb7ed'
         response = self.create_invalid_resource(invalid_name, valid_ticket)
         self.assertEqual(response.status_code, 404)
-        
+
+
 class GetPersonTest(TestCase, GetInfoTestFucntion):
+    """Test class for getting information of person"""
+
     def get_valid_person_by_name(self):
+        # Get informaiton using name
         name = 'vamshi'
         response = self.get_resource(name)
         self.assertEqual(response.status_code, 200)
 
     def get_valid_person_by_ticket(self):
+        # Get informaiton using ticket_id
         ticket_id = '2e6e79c4-aa80-406a-9517-9b75552eb7ed'
         response = self.get_resource(ticket_id)
-        self.assertEqual(response.status_code, 200)   
+        self.assertEqual(response.status_code, 200)
 
     def get_valid_person_by_seat(self):
+        # Get informaiton using seat_no
         seat_no = 1
         response = self.get_resource(seat_no)
-        self.assertEqual(response.status_code, 200)   
+        self.assertEqual(response.status_code, 200)
+
 
 class VacateTest(TestCase, VacateTestFuctions):
-    """Test class for vacating seat""" 
+    """Test class for vacating seat"""
+
     def test_create_valid_vacate(self):
         # Passing valid seat_no
         # vacating seat_no present in model
         response = self.delete_resource(4)
         self.assertEqual(response.status_code, 200)
- 
+
     def test_create_invalid_vacate(self):
         # Vacating the seat which is not allotted.
         # vacating seat_no not allocated
