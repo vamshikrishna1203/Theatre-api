e9c3731ef6ff7b1fa2c20661dd2500477f46c4d1
vamshi <vamshiguptha1203@gmail.com>
2021-03-10 00:12:21 +0530

    style: add comments

--- managerapp/testfunctions.py
+++ managerapp/testfunctions.py
@@ -4,16 +4,19 @@ BASE_URL = 'http://127.0.0.1:8000/'
 
 
 class GetInfoTestFucntion:
+    """TestCase Fucntion for  /get_info/<NAME or SEATNUM or TICKETID>"""
+
     def get_resource(self, data):
-        # TestCase for  /get_info/<NAME or SEATNUM or TICKETID>
         # Takes SEATNUM or TICKETID or NAME
         response = requests.get(BASE_URL + 'get_info/' + str(data) + '/')
         return response
 
 
 class OccupyTestFuctions:
+    """TestCase fucntions for /occupy/"""
+
     def create_valid_resource(self, name, ticket_id):
-        # TestCase for /occupy/
+        # Creating seat using valid resource
         # Formating data according to Consumer object present in model objects
         data = {
             'name': name,
@@ -23,7 +26,7 @@ class OccupyTestFuctions:
         return response
 
     def create_invalid_resource(self, name, ticket_id):
-        # TestCase for /occupy/
+        # Creating seat using invalid resource
         # Formating data according to Consumer object present in model objects
         data = {
             'name': name,
@@ -34,7 +37,9 @@ class OccupyTestFuctions:
 
 
 class VacateTestFuctions:
+    """TestCase fucntions for /vacate/"""
+
     def delete_resource(self, id):
-        # TestCase for /vacate/
+        # Vacating resouse using seat_no
         response = requests.delete(BASE_URL + 'vacate/' + str(id) + '/')
         return response
